2ceca7a5d782a6f200b96714772fffaa953b9681
anshoryihsan <anshoryihsan@gmail.com>
2021-11-11 13:22:56 +0700

    update refactorCode

--- refactorCode.js
+++ refactorCode.js
@@ -1,23 +1,12 @@
-if(str.length > 0){
-let indexFirstBracketFound = str.indexOf("(");
-if(indexFirstBracketFound >= 0){
-let wordsAfterFirstBracket = str.substr( indexFirstBracketFound );
-if(wordsAfterFirstBracket){
-wordsAfterFirstBracket = wordsAfterFirstBracket.substr(1);
-let indexClosingBracketFound = wordsAfterFirstBracket.indexOf(")");
-if(indexClosingBracketFound >= 0){
-return wordsAfterFirstBracket.substring(0,indexClosingBracketFound);
+function findFirstStringInBracket(str) {
+  if (str.length > 0) {
+    let indexFirstBracketFound = str.indexOf("(") + 1;
+    let wordsAfterFirstBracket = str.substr(indexFirstBracketFound);
+    let indexClosingBracketFound = wordsAfterFirstBracket.indexOf(")");
+    str = str.substr(indexFirstBracketFound, indexClosingBracketFound);
+    return str;
+  } else {
+    return "";
+  }
 }
-else{
-return '';
-}
-}else{
-return '';
-}
-}else{
-return '';
-}
-}else{
-return '';
-}
-}
+console.log(findFirstStringInBracket("asdawdaw(sdisi)sdawdawda"));
